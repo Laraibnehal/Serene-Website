4a8f2931a53dae77488077058ffe3c39e07a9e93
Laraib <laraibmallick7@gmail.com>
2023-12-25 14:16:42 +0530

    backend index.js updated

--- Backend/index.js
+++ Backend/index.js
@@ -1,6 +1,11 @@
-import { Express } from "express";
+require('dotenv').config()
+import express from "express"
 import cors from "cors";
+const port =4000;
 const app = express()
 app.use(cors());
+app.get('/',(req,res)=>{
+    res.send('Hello World!')
+})
 
-app.listen(3000,()=>console.log("App is running"));
+app.listen(process.env.PORT,()=>console.log("App is running on ${port}"));
