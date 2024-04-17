1d85ac3b8f78a4e9808ebcdca22505fd5e4e00c1
laraib <95610027+Laraibnehal@users.noreply.github.com>
2024-04-17 20:55:42 +0530

    Update Cart.jsx

--- src/components/Cart.jsx
+++ src/components/Cart.jsx
@@ -1,50 +1,50 @@
-import React , {useState} from 'react'
+// import React , {useState} from 'react'
 
-function Cart() {
-  // const [cartItems, setCartItems] = useState([]);
-  const [currentItemIndex, setCurrentItemIndex] = useState(0);
+// function Cart() {
+//   // const [cartItems, setCartItems] = useState([]);
+//   const [currentItemIndex, setCurrentItemIndex] = useState(0);
 
-  const currentItem = items[currentItemIndex];
+//   const currentItem = items[currentItemIndex];
 
-  const addToCart = (item) => {
+//   const addToCart = (item) => {
 
-    console.log('Added to cart:', currentItem.name);
+//     console.log('Added to cart:', currentItem.name);
  
-  };
-  const handleNextItem = () => {
-    setCurrentItemIndex((prevIndex) => (prevIndex + 1) % items.length);
-  };
+//   };
+//   const handleNextItem = () => {
+//     setCurrentItemIndex((prevIndex) => (prevIndex + 1) % items.length);
+//   };
 
-  const handlePreviousItem = () => {
-    setCurrentItemIndex((prevIndex) =>
-      prevIndex === 0 ? items.length - 1 : prevIndex - 1
-    );
-  };
-  return (
-    <div className='w-screen'>
-    <div className="w-full h-screen flex flex-col md:flex-row items-center justify-center overflow-hidden ">
-        <img src={currentItem.image} alt={currentItem.name} className="w-full md:w-1/2 overflow-hidden object-cover bg-slate-500" />
+//   const handlePreviousItem = () => {
+//     setCurrentItemIndex((prevIndex) =>
+//       prevIndex === 0 ? items.length - 1 : prevIndex - 1
+//     );
+//   };
+//   return (
+//     <div className='w-screen'>
+//     <div className="w-full h-screen flex flex-col md:flex-row items-center justify-center overflow-hidden ">
+//         <img src={currentItem.image} alt={currentItem.name} className="w-full md:w-1/2 overflow-hidden object-cover bg-slate-500" />
    
-        <p>{currentItem.rating}</p>
-    <h3 className='text-3xl m-6 '>{currentItem.name} Eau de Parfum</h3>
-        <p m-6>{currentItem.quantity}</p>
-        <p>{currentItem.description}</p>
-        <p>{currentItem.cost}</p>
+//         <p>{currentItem.rating}</p>
+//     <h3 className='text-3xl m-6 '>{currentItem.name} Eau de Parfum</h3>
+//         <p m-6>{currentItem.quantity}</p>
+//         <p>{currentItem.description}</p>
+//         <p>{currentItem.cost}</p>
       
 
-    <br/>
-        <p>Price: ${currentItem.price}</p>
-        <button onClick={addToCart}>Add to Cart</button>
+//     <br/>
+//         <p>Price: ${currentItem.price}</p>
+//         <button onClick={addToCart}>Add to Cart</button>
           
-        <div>
-        <button onClick={handlePreviousItem}>Previous Item</button>
-        <button onClick={handleNextItem}>Next Item</button>
-      </div>
-    </div>
+//         <div>
+//         <button onClick={handlePreviousItem}>Previous Item</button>
+//         <button onClick={handleNextItem}>Next Item</button>
+//       </div>
+//     </div>
 
  
-  </div>
-  )
-}
+//   </div>
+//   )
+// }
 
-export default Cart
+// export default Cart
