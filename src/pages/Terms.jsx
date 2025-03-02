8e1006df08245ffa32bcccea74af4a477d1563ee
laraib <95610027+Laraibnehal@users.noreply.github.com>
2025-03-02 15:27:31 +0530

    Update Terms.jsx

--- src/pages/Terms.jsx
+++ src/pages/Terms.jsx
@@ -12,7 +12,7 @@ function Termsconditions() {
     <div className='py-4 px-9 mx-9 text-center my-6 bg-slate-300 '>
       
     <p className=' px-6 my-3'>Welcome to Perfume Serene. These Terms and Conditions govern your use of the Perfume Serene website located at www.perfumeserene.com (the “Site”) and your relationship with Perfume Serene (referred to as “Perfume Serene”, “we” or “us”). Please read them carefully as they affect your rights and liabilities under the law. If you do not agree to these Terms and Conditions, please do not register for or use the Site. In these Terms and Conditions, “you” and “your” means any person who accesses and uses the Site or the services offered on the Site.</p>
-    <p className='  mx-5 py-2 px-3 bg-red-100 md:w-1/4 w-1/2 '>Registration</p>
+    <p className='  mx-5 py-2 px-3 bg-red-100 md:w-1/2 w-1/2 '>Registration</p>
     <p className='mx-5 py-6  px-6 my-3'>To use some of the services or features made available to you on this Site you will need to register. When you register you are required to provide information about yourself that is true, accurate, current, and complete in all respects. Should any of your registration information change, please notify us immediately at the following e-mail address <a className ="text-teal-500"href="mailto:perfumeserene@gmail.com">Send mail</a></p>
     <p className=' px-6 my-3'>The account password you provide should be unique and kept secure, and you must notify Perfume Serene immediately of any breach of security or unauthorized use of your account.</p>
     <p className='bg-red-100 md:w-1/2 w-1/2  text-center justify-self-center mx-5 py-2 px-3'>Eligibility to Purchase</p>
@@ -20,7 +20,7 @@ function Termsconditions() {
     <p className='text-justify  text-sm font-medium tracking-wide font-mono px-6 my-3'>1. Register by providing your real name, phone number, e-mail address, payment details and other requested information</p>
     <p className='text-justify text-sm font-medium tracking-wide font-mono px-6 my-3'>2. Be over 18 years of age</p>
     <p className='text-justify  text-sm font-medium tracking-wide font-mono px-6 my-3'>By making an offer to buy a product, you specifically authorize us to transmit information (including any updated information) or to obtain information about you from third parties from time to time, including but not limited to your debit or credit card number or credit reports, to authenticate your identity, to validate your credit card, to obtain an initial credit card authorization and to authorize individual purchase transactions.</p>
-    <p className='mx-5 py-2 px-3 bg-red-100 md:w-1/4 w-1/2 text-center justify-self-center'>Order Acceptance</p>
+    <p className='mx-5 py-2 px-3 bg-red-100 md:w-1/2 w-1/2 text-center justify-self-center'>Order Acceptance</p>
     <p className='text-justify px-6 my-3 text-sm font-medium tracking-wide font-mono'>All orders are subject to acceptance and availability. When you place an order on our Site we will send you an email confirming receipt of your order. This email is only an acknowledgement for information purposes and it does not constitute acceptance of your order by us. Your order will be accepted and the contract between you and Perfume Serene will be completed when we send you an email to confirm the goods have been dispatched to you. Only the goods listed in the confirmation email sent at the time of dispatch are included in the contract formed.</p>
    
     <p className=' mx-5 py-2 px-3 bg-red-100 md:w-1/2 w-1/2  '>Acceptable Payment Methods</p>
@@ -34,12 +34,12 @@ function Termsconditions() {
     <p className='text-justify px-6 my-3 text-sm font-medium tracking-wide font-mono'>You must take care when opening the product so as not to damage it, particularly when using any sharp instruments.</p>
     <p className='text-justify px-6 my-3 text-sm font-medium tracking-wide font-mono'>You shall ensure that you are ready for safe receipt of the product without undue delay and at any time reasonably specified by us.</p>
 
-    <p className='bg-red-100 md:w-1/4 w-1/2  text-center justify-self-center mx-5 py-2 px-3'>Cancellation of Order</p>
+    <p className='bg-red-100 md:w-1/2 w-1/2  text-center justify-self-center mx-5 py-2 px-3'>Cancellation of Order</p>
     <p className='text-justify px-6 my-3 text-sm font-medium tracking-wide font-mono'>You may cancel your order any time before the order has been processed. Once the order has been shipped, you will receive a shipping confirmation via email that will contain the tracking information. As soon as the product has been shipped, our exchange policy will be applicable.</p>
-    <p className='bg-red-100 md:w-1/4 w-1/2  text-center justify-self-center mx-5 py-2 px-3' >Cookie/Tracking Technology</p>
+    <p className='bg-red-100 md:w-1/2 w-1/2  text-center justify-self-center mx-5 py-2 px-3' >Cookie/Tracking Technology</p>
     <p className='text-justify px-6 my-3 text-sm font-medium tracking-wide font-mono'>The Site may use cookie and tracking technology depending on the features offered. Cookie and tracking technology are useful for gathering information such as browser type and operating system, tracking the number of visitors to the Site, and understanding how visitors use the Site. Cookies can also help customize the Site for visitors. Personal information cannot be collected via cookies and other tracking technology; however, if you previously provided personally identifiable information, cookies may be tied to such information. Aggregate cookie and tracking information may be shared with third parties.</p>
 
-    <p className='mx-5 py-2 px-3 bg-red-100 md:w-1/4 w-1/2  text-center justify-self-center'>Product Availability</p>
+    <p className='mx-5 py-2 px-3 bg-red-100 md:w-1/2 w-1/2  text-center justify-self-center'>Product Availability</p>
     <p className='text-justify px-6 my-3 text-sm font-medium tracking-wide font-mono'>Lead time is dependent on stock availability. Should we have stock available, orders will be dispatched the same day provided payment is validated/received before 14:00 hours.</p>
     <p className='text-justify px-6 my-3 text-sm font-medium tracking-wide font-mono'>Should the product not be in stock please allow 10 working days plus freight transit time. This will allow for manufacturing and delivery to our warehouse.</p>
     <p className='text-justify px-6 my-3 text-sm font-medium tracking-wide font-mono'>Every effort is made to effect shipment of your order timeously however unforeseen circumstances may cause delays. Should we be faced with such an occurrence we will contact you via email and provide a revised shipping schedule.</p>
